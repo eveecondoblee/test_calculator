7df304a180b990a2a2baea259f09ff6c4dc84783
eveecondoblee <o1100101evee@hotmail.com>
2025-04-21 20:59:15 -0600

    Añadir función de raíz cuadrada a la calculadora

--- calculator.py
+++ calculator.py
@@ -11,3 +11,11 @@ def divide(a, b):
   if b == 0:
     raise ValueError("Cannot divide by zero")
   return a / b
+
+def square(n, i=100):
+  if n < 0:
+    raise ValueError("Cannot calculate square root of negative number")
+  approx = n
+  for _ in range(i):
+    approx = 0.5 * (approx + n / approx)
+  return approx
