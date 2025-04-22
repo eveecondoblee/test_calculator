95c9fb106dadb04cf5260bbb7e8a4203ed015db0
eveecondoblee <o1100101evee@hotmail.com>
2025-04-21 21:02:37 -0600

    Añadir función exponencial a la calculadora

--- calculator.py
+++ calculator.py
@@ -19,3 +19,13 @@ def square(n, i=100):
   for _ in range(i):
     approx = 0.5 * (approx + n / approx)
   return approx
+
+def exponential(x, i=100):
+  result = 1.0
+  term = 1.0
+  for n in range(1, i):
+    term *= x / n
+    result += term
+    if abs(term) < 0.001:
+      break
+  return result
